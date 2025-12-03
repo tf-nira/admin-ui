0375683312d23cce2681a015dcda13c7656ee4fc
Jagadeesh-T440 <jagadeesh.thiyagarajan@technoforte.co.in>
2025-12-03 12:32:53 +0530

    Update header.service.ts

--- admin-ui/src/app/core/services/header.service.ts
+++ admin-ui/src/app/core/services/header.service.ts
@@ -63,6 +63,14 @@ export class HeaderService {
     }    
   }
 
+  public getRawRoles(): string[] {
+  return this.roles
+    .split(',')
+    .map(r => r.trim())
+    .filter(r => r.length > 0); 
+  }
+
+
 }
 
 
